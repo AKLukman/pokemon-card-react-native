26dc1c49536912581281c8d4da643d0c488d3c3e
AKLukman <abulkashemlukman@gmail.com>
2025-03-17 20:58:08 +0000

    updated

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  FlatList,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+  SectionList,
+} from "react-native";
+import PokemanCard from "./components/PokemanCard";
+import pokemonData from "./data.json";
+import groupedData from "./group-data.json";
 
 export default function App() {
+  const emmptyImage = require("./assets/bulbasaur.png");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <ScrollView> */}
+      {/* <PokemanCard {...charmanderData}></PokemanCard>
+        <PokemanCard {...squirtleData}></PokemanCard>
+        <PokemanCard {...bulbasaurData}></PokemanCard>
+        <PokemanCard {...pikachuData}></PokemanCard> */}
+      {/* {pokemonData.map((pokemon) => {
+          return <PokemanCard {...pokemon} key={pokemon.id}></PokemanCard>;
+        })} */}
+      {/* </ScrollView> */}
+
+      {/* <FlatList
+        data={pokemonData}
+        renderItem={({ item }) => {
+          return <PokemanCard {...item} key={item.id}></PokemanCard>;
+        }}
+        keyExtractor={(item, index) => item.id.toString()}
+        // horizontal={true}
+        // ItemSeparatorComponent={<View style={{ height: 16 }}></View>}
+        ListEmptyComponent={
+          <View style={styles.emptyContainer}>
+            <Text style={styles.emptyText}>No Pokémon found!</Text>
+          </View>
+        }
+        ListHeaderComponent={
+          <Text style={styles.listHeaderText}>Pokemon List</Text>
+        }
+        ListFooterComponent={
+          <Text style={styles.listFooterText}>End of the list</Text>
+        }
+      /> */}
+      {/* we can set the title on group of data */}
+      <SectionList
+        sections={groupedData}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.cardText}>{item}</Text>
+            </View>
+          );
+        }}
+        renderSectionHeader={({ section }) => {
+          return (
+            <View>
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: "bold",
+                }}
+              >
+                {section.type}
+              </Text>
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={{ height: 16 }}></View>}
+        SectionSeparatorComponent={() => <View style={{ height: 16 }}></View>}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f5f5f5",
+    // paddingTop: Platform.OS === "android" ? 25 : 0,
+    paddingTop: StatusBar.currentHeight,
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  emptyText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#FF4500", // Fire-like theme color
+  },
+  listHeaderText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "green",
+  },
+  listFooterText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "red",
+    marginBottom: 16,
+  },
+  card: {
+    padding: 16,
+    borderRadius: 4,
+    borderWidth: 1,
+    marginHorizontal: 16,
+  },
+  cardText: {
+    fontSize: 20,
   },
 });
---
+++ components/PokemanCard.js
@@ -0,0 +1,115 @@
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
+import getTypeDetails from "../utils/typeDetails";
+
+export default function PokemanCard({
+  name,
+  image,
+  type,
+  hp,
+  moves,
+  weaknesses,
+}) {
+  const { borderColor, emoji } = getTypeDetails(type);
+  return (
+    <View style={styles.card}>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.hp}>❤️HP: {hp}</Text>
+      </View>
+      <View>
+        <Image
+          source={image}
+          accessibilityLabel={`${name} pokemon`}
+          style={styles.image}
+          resizeMode="contain"
+        ></Image>
+      </View>
+      <View style={styles.typeContainer}>
+        <View style={[styles.badge, { borderColor }]}>
+          <Text style={styles.typeEmoji}>{emoji}</Text>
+          <Text style={styles.typeText}>{type}</Text>
+        </View>
+      </View>
+      <View style={styles.movesContainer}>
+        <Text style={styles.movesText}>Moves: {moves.join(", ")}</Text>
+      </View>
+      <View style={styles.weaknessContainer}>
+        <Text style={styles.weakneesText}>
+          weaknesses: {weaknesses.join(", ")}
+        </Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderRadius: 16,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  name: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  hp: {
+    fontSize: 22,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
+  },
+  typeContainer: {
+    marginBottom: 40,
+    alignItems: "center",
+  },
+  badge: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4,
+  },
+  typeEmoji: {
+    fontSize: 20,
+    marginRight: 12,
+  },
+  typeText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  movesContainer: {
+    marginBottom: 16,
+  },
+  movesText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  weaknessContainer: {
+    marginBottom: 8,
+  },
+  weakneesText: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
